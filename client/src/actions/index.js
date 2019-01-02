c20c3b6ef8021c54b73c3dfe16c79cbf8effb774
Vina Compson <vv.compson@icloud.com>
2019-01-02 13:46:10 +1100

    add reduxThunk for action creator

---
+++ client/src/actions/index.js
@@ -0,0 +1,10 @@
+//import axios to make ajax request
+import axios from 'axios';
+import { FETCH_USER } from './types';
+
+const fetchUser = () => {
+    return function(dispatch) {
+        axios.get('/api/current_user')
+            .then(res => dispatch*{ type: FETCH_USER, payload: res });
+    }    
+};
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,9 +3,14 @@ import React, { Component } from 'react';
 class Header extends Component {
     render() {
         return (
-            <div>
-                Header
-            </div>
+            <nav>
+                <div className="nav-wrapper">
+                <a href="/" className="left brand-logo">Emaily</a>
+                <ul id="nav-mobile" className="right">
+                    <li><a href="/">Login with Google</a></li>
+                </ul>
+                </div>
+            </nav>
         )
     }
 }
