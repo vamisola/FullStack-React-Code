50d1bf829c95c7b587891a096dd3f0f9194b247c
Vina Compson <vv.compson@icloud.com>
2019-01-06 15:13:31 +1100

    add switch case when logging

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
 class Header extends Component {
+    renderContent() {
+        switch(this.props.auth) {
+            case null:
+                return;
+            case false:
+                return <li><a href="/auth/google">Login with Google</a></li>;
+            default:
+                return <li><a href="/">Logout</a></li>;
+        }
+    }
     render() {
         return (
             <nav>
                 <div className="nav-wrapper">
                 <a href="/" className="left brand-logo">Emaily</a>
-                <ul id="nav-mobile" className="right">
-                    <li><a href="/">Login with Google</a></li>
+                <ul className="right">
+                    {this.renderContent()}
                 </ul>
                 </div>
             </nav>
@@ -15,4 +25,8 @@ class Header extends Component {
     }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+    return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,8 +6,13 @@ module.exports = (app) => {
   app.get('/auth/google',
     passport.authenticate('google', { scope: ['profile', 'email'] }));
 
-  app.get('/auth/google/callback',
-    passport.authenticate('google'));
+  app.get(
+    '/auth/google/callback', 
+    passport.authenticate('google'),
+    (req, res) => {
+      res.redirect('/surveys');
+    }
+  );
 
   app.get('/api/logout', (req, res) => {
     req.logout();
